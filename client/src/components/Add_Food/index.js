7338ccec03f23b5ef1cb764d3b79a6a40682e0a7
StevenVC-P <77998885+StevenVC-P@users.noreply.github.com>
2021-07-09 15:36:30 -0500

    moved dropdown to its own compononent and switch the to te combobox example

--- client/src/components/Add_Food/index.js
+++ client/src/components/Add_Food/index.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
-import {render} from 'react-dom';
-import Downshift from 'downshift';
+import DropdownCombobox from '../DropdownComboBox'
 import Container from "../Container";
 import "./style.css";
 import API from "../../utils/API";
 
 function AddFood (props) {
+    const [selection, addSelection] = useState("");
     const [newFoodName, addNewFoodName] = useState({});
     const [newFoodAmount, addNewFoodAmount] = useState({});
     const [newAmountType, addNewAmountType] = useState({});
@@ -19,6 +19,11 @@ function AddFood (props) {
             setItems(res.data)
         })
     },[count])
+
+    function handleSelection(e) {
+        const { name, value } = e.target;
+        addSelection({...selection, [name]: value})
+    };
 
     function handleInputChange(e) {
         const { name, value } = e.target;
@@ -37,25 +42,35 @@ function AddFood (props) {
 
     const onSubmit = (e) => {
         e.preventDefault();
-        console.log(newFoodName)
-        if(newFoodName.newFoodName && newFoodAmount.newFoodAmount && newAmountType.newAmountType){
-            API.addFood({
-                foodName: newFoodName.newFoodName,
-                foodAmount: newFoodAmount.newFoodAmount,
-                amountType: newAmountType.newAmountType
-            })
-            .then(res =>{
-                console.log('New Food Res', res)
-                if (res.status === 200){
-                    console.log('Success', res.data)
-                    API.locateFood(res.data)
-                } else {
-                    console.log(res.status)
-                }
-            
-            })
-            .catch(err => console.log("Food Add Error", err));
+        console.log(selection)
+        console.log(selection.value)
+        if(selection.value){
+            if(newFoodName.newFoodName && newFoodAmount.newFoodAmount && newAmountType.newAmountType){
+                API.getLocationNames(selection.value)
+                .then(res => {
+                    if(res.status === 200){
+                        API.addFood({
+                            foodName: newFoodName.newFoodName,
+                            foodAmount: newFoodAmount.newFoodAmount,
+                            amountType: newAmountType.newAmountType
+                        })
+                        .then(res =>{
+                            console.log('New Food Res', res)
+                            if (res.status === 200){
+                                console.log('Success', res.data)
+                                API.locateFood(res.data)
+                            } else {
+                                console.log(res.status)
+                            }
+                        })
+                        .catch(err => console.log("Food Add Error", err));
+                    }
+                })
+            }
+        } else {
+            console.log("Please Select a Location")
         }
+
     };
 
     return(
@@ -66,56 +81,7 @@ function AddFood (props) {
                     <input type="text" name="newFoodName" placeholder="Food Name" onChange={handleInputChange} id="newFoodName"/>
                     <input type="text" name="newFoodAmount" placeholder="#" onChange={handleAmountChange} id="newFoodAmount"/>
                     <input type="text" name="newAmountType" placeholder="#" onChange={handleAmountType} id="newAmountType"/>
-                    <Downshift
-                        onChange={selection =>
-                        alert(selection ? `You Selected ${selection.value}`: `Selection Cleared`)
-                    }
-                    itemToString={item=> (item ? item.value:'')}
-                    >
-                        {({
-                            getInputProps,
-                            getItemProps,
-                            getLabelProps,
-                            getMenuProps,
-                            isOpen,
-                            inputValue,
-                            highlightedIndex,
-                            selectedItem,
-                            getRootProps,
-                            }) => (
-                                <div>
-                                    <label {...getLabelProps()}>Add a Location  </label>
-                                    <div
-                                        style={{display:'inline-block'}}
-                                        {...getRootProps({}, {suppressRefErrr: true})}
-                                    >
-                                        <input {...getInputProps()} />
-                                    </div>
-                                    <ul {...getMenuProps()}>
-                            {isOpen
-                                ? items
-                                    .filter(item => !inputValue || item.value.includes(inputValue))
-                                    .map((item, index) => (
-                                    <li
-                                        {...getItemProps({
-                                        key: item.value,
-                                        index,
-                                        item,
-                                        style: {
-                                            backgroundColor:
-                                            highlightedIndex === index ? 'lightgray' : 'white',
-                                            fontWeight: selectedItem === item ? 'bold' : 'normal',
-                                        },
-                                        })}
-                                    >
-                                        {item.value}
-                                    </li>
-                                    ))
-                                : null}
-                            </ul>
-                        </div>
-                        )}
-                    </Downshift>
+                    <DropdownCombobox />
 
                     <button type="submit" className="submit" onClick={onSubmit} id="btnFood">+</button>
                 </form>
